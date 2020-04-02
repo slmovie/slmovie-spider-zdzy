64e080f7898034f88a038108e3f17c1d88f6303a
Bao Jun <baojun.cz@gmail.com>
2020-04-02 19:46:58 +0800

    refactor: delete date-format

--- src/utils/log-utils.js
+++ src/utils/log-utils.js
@@ -1,4 +1,14 @@
-const getTimeFormat = require('./date-format').getTimeFormat;
+
+const getTimeFormat = (time) => {
+	let date = new Date(time);
+	let Y = date.getFullYear() + '-';
+	let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+	let D = date.getDate() + ' ';
+	let h = date.getHours() + ':';
+	let m = date.getMinutes() + ':';
+	let s = date.getSeconds();
+	return Y + M + D + h + m + s;
+};
 
 exports.log = (log, time) => {
 	if (time) {
