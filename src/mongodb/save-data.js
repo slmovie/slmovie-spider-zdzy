312ea0de08f23a812ceb1b853bad9c8f9d91744d
BaojunCZ <baojun.cz@gmail.com>
2020-04-02 23:30:14 +0800

    feat: save new movie

--- src/mongodb/save-data.js
+++ src/mongodb/save-data.js
@@ -1,6 +1,7 @@
 const DBAddress = require('./db-constans').DBAddress;
 const mongoose = require('mongoose');
 const log = require('../utils/log-utils');
+const MovieSchema = require('./detail-schema').MovieSchema;
 
 const db = mongoose.createConnection(DBAddress + '/movies', {
 	useNewUrlParser: true,
@@ -11,3 +12,35 @@ db.catch((error) => {
 	log(error);
 	process.exit(0);
 });
+
+function saveMovie(movie,type) {
+	return new Promise((resolve, reject) => {
+		const model = db.model('Movie', MovieSchema);
+		findOneByID(model, movie.id)
+			.then((data) => {
+				//有数据，检查更新
+			})
+			.catch((error) => {
+				//没有查到，直接添加
+				model.create(detail, (error) => {
+					if (error) {
+						reject(error);
+					} else {
+						resolve();
+					}
+				});
+			});
+	});
+}
+
+function findOneByID(model, id) {
+	return new Promise((resolve, reject) => {
+		model.findOne({ id: id }, (error, doc) => {
+			if (error || doc == null) {
+				reject();
+			} else {
+				resolve(doc);
+			}
+		});
+	});
+}
