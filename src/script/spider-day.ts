58553095aff170485a95ffec8f6667d5a94186f1
Bao Jun <baojun.cz@gmail.com>
2020-04-13 20:25:39 +0800

    feat: spider dayly

--- src/script/spider-day.ts
+++ src/script/spider-day.ts
@@ -1,14 +1,17 @@
 import { spiderDay } from "../spider";
+import schedule from "node-schedule";
 import { log } from "../utils/log-utils";
 import { setNewMovies } from "../setting/home-hot";
 
-spiderDay().then(() => {
-    log("Dayly spider finish")
-    setNewMovies().then(() => {
-        log("update home page finish")
-        process.exit(0)
-    }).catch(error => {
-        log(error)
-        process.exit(0)
+schedule.scheduleJob('* * 5 * *', () => {
+    spiderDay().then(() => {
+        log("Dayly spider finish")
+        setNewMovies().then(() => {
+            log("update home page finish")
+            process.exit(0)
+        }).catch(error => {
+            log(error)
+            process.exit(0)
+        })
     })
 })
